9b9aaf63c55fbefd49cd091abfe6070a48769366
Rafail Ferre <qa.rafailferre@gmail.com>
2024-07-08 22:25:20 -0400

    create type for api request, response; add negative test for auth

--- specs/auth.spec.ts
+++ specs/auth.spec.ts
@@ -2,17 +2,41 @@
 import * as supertest from "supertest";
 const request = supertest("http://localhost:3000");
 
+import { AuthResponseBody } from "../api/auth";
+
 describe("AUTHENTICATION & AUTHORIZATION", () => {
-  it("Sign in with valid credentials", async () => {
-    const res = await request.post("/auth").send({           // await request("http://localhost:3000").post("/auth").send({
-      login: "admin",
-      password: "admin"
+  describe("POSITIVE", () => {
+    let res: {status : number, body: AuthResponseBody}
+    beforeEach(async() => {
+            res = await request.post("/auth").send({        // await request("http://localhost:3000").post("/auth").send({
+            login: "admin",
+            password: "admin",
+          });
     });
 
-    expect(res.status).toBe(200);
-    expect(typeof(res.body.token)).toBe('string');
+    it("Sign in with valid credentials", async () => {
+      expect(res.status).toEqual(200);
+      expect(res.body.token).toEqual(expect.any(String));
+      expect(res.body).toEqual<AuthResponseBody>({
+        token: expect.any(String),
+      });
+      console.log(res.body.token, "------TOKEN------");
+    });
 
-    console.log(res.body.token, '------TOKEN------');
-    
+  });
+  describe("NEGATIVE", () => {
+    it("Sign in with invalid credentials", async () => {
+      const res = await request.post("/auth").send({
+        // await request("http://localhost:3000").post("/auth").send({
+        login: "admin",
+        password: "invalid_password"
+      });
+
+      expect(res.status).toEqual(404);
+      expect(res.body.message).toEqual('Wrong login or password.');
+      expect(res.body.message).toEqual(expect.any(String));
+
+      console.log(res.body.message, "------ MESSAGE ------");
+    });
   });
 });
